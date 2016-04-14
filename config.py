66cbec935f56d7651ac64b90021d6bc5b217bc02
Kyle Cesmat <kyle@kylecesmat.com>
2016-04-14 16:31:37 -0700

    more the values into a config file

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration for the moisture.py script
+
+smtp = dict(
+    username = 'null', # This is the username used to login to your SMTP provider
+    password = 'null', # This is the password used to login to your SMTP provider
+    host = 'host', # This is the host of the SMTP provider
+    port = 25, # This is the port that your SMTP provider uses
+    sender = 'null' # This is the FROM email address
+    receiver = 'null', # This is the TO email address
+)
--- moisture.py
+++ moisture.py
@@ -5,21 +5,21 @@
 import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
 import smtplib # This is the SMTP library we need to send the email notification
 import time # This is the time library, we need this so we can use the sleep function
+import config # Our config file (not committed)
 
 # Define some variables to be used later on in our script
 
 # You might not need the username and password variable, depends if you are using a provider or if you have your raspberry pi setup to send emails
 # If you have setup your raspberry pi to send emails, then you will probably want to use 'localhost' for your smtp_host
 
-smtp_username = "enter_username_here" # This is the username used to login to your SMTP provider
-smtp_password = "enter_password_here" # This is the password used to login to your SMTP provider
-smtp_host = "enter_host_here" # This is the host of the SMTP provider
-smtp_port = 25 # This is the port that your SMTP provider uses
+smtp_username = config.smtp['username']
+smtp_password = config.smtp['password']
+smtp_host = config.smtp['host']
+smtp_port = config.smtp['port']
+smtp_sender = config.smtp['sender']
+smtp_receivers = config.smtp['receiver']
 
-smtp_sender = "sender@email.com" # This is the FROM email address
-smtp_receivers = ['receiver@email.com'] # This is the TO email address
-
-# The next two variables use triple quotes, these allow us to preserve the line breaks in the string. 
+# The next two variables use triple quotes, these allow us to preserve the line breaks in the string.
 
 # This is the message that will be sent when NO moisture is detected
 
@@ -45,14 +45,14 @@ def sendEmail(smtp_message):
 	try:
 		smtpObj = smtplib.SMTP(smtp_host, smtp_port)
 		smtpObj.login(smtp_username, smtp_password) # If you don't need to login to your smtp provider, simply remove this line
-		smtpObj.sendmail(smtp_sender, smtp_receivers, smtp_message)         
+		smtpObj.sendmail(smtp_sender, smtp_receivers, smtp_message)
 		print "Successfully sent email"
 	except SMTPException:
 		print "Error: unable to send email"
 
 # This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
-def callback(channel):  
+def callback(channel):
 	if GPIO.input(channel):
 		print "LED off"
 		sendEmail(message_dead)
@@ -75,5 +75,5 @@ GPIO.add_event_callback(channel, callback)
 
 # This is an infinte loop to keep our script running
 while True:
-	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
-	time.sleep(0.1)
+	# This line simply tells our script to wait 1 second, this is so the script doesnt hog all of the CPU
+	time.sleep(1)
